8b1526e5b1cbdaf33b43ee10cd4e41a264c529e3
douglastofoli <tofoli.douglas@hotmail.com>
2023-06-06 14:02:19 -0300

    create seeds with random users

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,41 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Peapescarte.Users.Users
+
+names = ["Alice", "Bob", "Charlie", "David", "Eva", "Frank", "Grace", "Hannah", "Isaac", "Julia"]
+
+social_names = [
+  "Alex",
+  "Bailey",
+  "Cameron",
+  "Dakota",
+  "Elliott",
+  "Finley",
+  "Harper",
+  "Jordan",
+  "Kai",
+  "Lennon"
+]
+
+domains = ["example.com", "test.com", "dummy.net", "foobar.org"]
+
+users =
+  Enum.map(1..50, fn _ ->
+    name = Enum.random(names)
+    social_name = Enum.random(social_names)
+    email = "#{String.downcase(name)}@#{Enum.random(domains)}"
+    age = Enum.random(1..100)
+
+    %{
+      nome: name,
+      nome_social: social_name,
+      email: email,
+      idade: age
+    }
+  end)
+
+Enum.map(users, fn user ->
+  Users.create_user(user)
+end)
